f961b631507ecc7ef6ff39664d7a542845929495
KasraFnp <fnpkasra@gmail.com>
2025-09-27 21:53:57 +0330

    models

---
+++ todolist/app_todo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True , max_length=200)
+    completed = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    due_date = models.DateField(blank=True , null=True)
+    
+    def __str__(self):
+        return self.title
