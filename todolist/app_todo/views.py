24df98be3cc23829a2871f3bca69aff636db458e
KasraFnp <fnpkasra@gmail.com>
2025-09-27 22:59:28 -0900

    Migrate todo templates and URLs to app_todo namespace
    Added new templates for task list, form, and delete confirmation under app_todo. Updated views to use the new template paths and changed the main urls.py to include app_todo.urls instead of todo.urls. This organizes the app structure under the app_todo namespace.

--- todolist/app_todo/views.py
+++ todolist/app_todo/views.py
@@ -6,7 +6,7 @@ from .forms import TaskForm
 
 def task_list(request):
     tasks = Task.objects.all().order_by('-created_date')
-    return render(request, 'todo/task_list.html', {'tasks': tasks})
+    return render(request, 'app_todo/task_list.html', {'tasks': tasks})
 
 def task_create(request):
     if request.method == 'POST':
@@ -16,7 +16,7 @@ def task_create(request):
             return redirect('task_list')
     else:
         form = TaskForm()
-    return render(request, 'todo/task_form.html', {'form': form})
+    return render(request, 'app_todo/task_form.html', {'form': form})
 
 def task_update(request, pk):
     task = get_object_or_404(Task, pk=pk)
@@ -27,11 +27,11 @@ def task_update(request, pk):
             return redirect('task_list')
     else:
         form = TaskForm(instance=task)
-    return render(request, 'todo/task_form.html', {'form': form})
+    return render(request, 'app_todo/task_form.html', {'form': form})
 
 def task_delete(request, pk):
     task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
         task.delete()
         return redirect('task_list')
-    return render(request, 'todo/task_confirm_delete.html', {'task': task})
+    return render(request, 'app_todo/task_confirm_delete.html', {'task': task})
