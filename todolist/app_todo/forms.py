9cc4d243e0ba2011c172ba0ff9821cec345799db
KasraFnp <fnpkasra@gmail.com>
2025-10-03 11:42:12 +0330

    Add file attachment support to tasks and improve UI
    Introduced an 'attachment' field to the Task model and form, allowing users to upload files with tasks. Updated task_form.html and base.html for improved layout and styling, and enhanced task_list.html for better task display and management.

--- todolist/app_todo/forms.py
+++ todolist/app_todo/forms.py
@@ -6,7 +6,11 @@ class TaskForm(forms.ModelForm):
         widget=forms.DateInput(attrs={'type': 'date'}),
         required=False
     )
+    attachment = forms.FileField(
+        required=False,
+        widget=forms.ClearableFileInput(attrs={'class': 'form-control'})
+    )
 
     class Meta:
         model = Task
-        fields = ['title', 'description', 'completed', 'due_date']
+        fields = ['title', 'description', 'completed', 'due_date', 'attachment']
--- todolist/app_todo/models.py
+++ todolist/app_todo/models.py
@@ -9,6 +9,7 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     created_date = models.DateTimeField(default=timezone.now)
     due_date = models.DateField(blank=True , null=True)
+    attachment = models.FileField(upload_to='attachments/' , blank=True , null=True)
     
     def __str__(self):
         return self.title
